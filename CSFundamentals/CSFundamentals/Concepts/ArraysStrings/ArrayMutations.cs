719da7eb73dd1da848f4c9a2eb44d238db35730f
Yessy Eckley <30677117+YessyEckley@users.noreply.github.com>
2020-09-21 00:34:15 -0600

    Array mutations

---
+++ CSFundamentals/CSFundamentals/Concepts/ArraysStrings/ArrayMutations.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace CSFundamentals.Concepts.ArraysStrings
+{
+    public class ArrayMutations<T>
+    {
+        public static T[] AddToStart(T[] a, T aValue)
+        {
+            var result = new T[a.Count() + 1];
+
+            Array.Copy(a, 0, result, 1, a.Count());
+            result[0] = aValue;
+
+            return result;
+        }
+
+        public static T[] AddToEnd(T[] a, T aValue)
+        {
+            var result = new T[a.Count() + 1];
+
+            Array.Copy(a, 0, result, 0, a.Count());
+            result[a.Count()] = aValue;
+
+            return result;
+        }
+
+        public static T[] InsertAt(T[] a, int index, T aValue)
+        {
+            var result = new T[a.Count() + 1];
+            var constIndex = 0;
+
+            for(var i = 0; i < a.Count() + 1; i++)
+            {
+                if(i == index)
+                {
+                    result[i] = aValue;
+                }
+                else
+                {
+                    result[i] = a[constIndex];
+                    constIndex++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ CSFundamentals/CSFundamentalsTests/Concepts/ArrayStrings/ArrayMutationsTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CSFundamentalsTests.Concepts.ArrayStrings
+{
+    public class ArrayMutationsTest
+    {
+        private readonly ITestOutputHelper _output;
+
+        public ArrayMutationsTest(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void AddToStartTest1()
+        {
+            var a = Array.ConvertAll("1 2 3 4 5".Split(" "), conv => Convert.ToInt32(conv));
+            var expected = Array.ConvertAll("6 1 2 3 4 5".Split(" "), conv => Convert.ToInt32(conv));
+            var actual = CSFundamentals.Concepts.ArraysStrings.ArrayMutations<int>.AddToStart(a, 6);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void AddToEndTest1()
+        {
+            var a = Array.ConvertAll("1 2 3 4 5".Split(" "), conv => Convert.ToInt32(conv));
+            var expected = Array.ConvertAll("1 2 3 4 5 6".Split(" "), conv => Convert.ToInt32(conv));
+            var actual = CSFundamentals.Concepts.ArraysStrings.ArrayMutations<int>.AddToEnd(a, 6);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void InsertAtTest1()
+        {
+            var a = Array.ConvertAll("1 2 3 4 5 6".Split(" "), conv => Convert.ToInt32(conv));
+            var expected = Array.ConvertAll("1 2 3 7 4 5 6".Split(" "), conv => Convert.ToInt32(conv));
+            var actual = CSFundamentals.Concepts.ArraysStrings.ArrayMutations<int>.InsertAt(a, 3, 7);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void InsertAtTest2()
+        {
+            var a = Array.ConvertAll("1 2 3 4 5 6".Split(" "), conv => Convert.ToInt32(conv));
+            var expected = Array.ConvertAll("1 2 3 4 5 6 7".Split(" "), conv => Convert.ToInt32(conv));
+            var actual = CSFundamentals.Concepts.ArraysStrings.ArrayMutations<int>.InsertAt(a, 6, 7);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
