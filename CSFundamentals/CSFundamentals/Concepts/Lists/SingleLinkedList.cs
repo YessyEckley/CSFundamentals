1ca8e58e0488664e9dfbf693a24d94e7ff5fdb89
Yessy Eckley <30677117+YessyEckley@users.noreply.github.com>
2020-09-23 01:44:34 -0600

    Linked list

--- CSFundamentals/CSFundamentals/Concepts/Lists/SingleLinkedList.cs
+++ CSFundamentals/CSFundamentals/Concepts/Lists/SingleLinkedList.cs
@@ -1,10 +1,75 @@
 ﻿using System;
 namespace CSFundamentals.Concepts.Lists
 {
-    public class SingleLinkedList
+    public class SingleLinkedList<T>
     {
-        public SingleLinkedList()
+        public Node<T> Head { get; set; }
+
+        public void Append(T data)
         {
+            if(Head == null)
+            {
+                Head = new Node<T>(data);
+
+                return;
+            }
+
+            var currentNode = Head;
+
+            while(currentNode.NextNode != null)
+            {
+                currentNode = currentNode.NextNode;
+            }
+
+            currentNode.NextNode = new Node<T>(data);
+        }
+
+        public void Prepend(T data)
+        {
+            var newHead = new Node<T>(data);
+
+            newHead.NextNode = Head;
+
+            Head = newHead;
+        }
+
+        public void Delete(T data)
+        {
+            if(Head == null)
+            {
+                return;
+            }
+
+            if(Head.Data.Equals(data))
+            {
+                Head = Head.NextNode;
+            }
+
+            var currentNode = Head;
+
+            while(currentNode.NextNode != null)
+            {
+                if(currentNode.NextNode.Data.Equals(data))
+                {
+                    currentNode.NextNode = currentNode.NextNode.NextNode;
+
+                    return;
+                }
+
+                currentNode = currentNode.NextNode;
+            }
+        }
+    }
+
+    public class Node<T>
+    {
+        public T Data { get; set; }
+
+        public Node<T> NextNode { get; set; }
+
+        public Node(T data)
+        {
+            Data = data;
         }
     }
 }
