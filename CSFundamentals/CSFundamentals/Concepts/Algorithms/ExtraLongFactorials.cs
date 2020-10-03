61d78a55537212e5c1ca46e70f647db98602cedd
Yessy Eckley <30677117+YessyEckley@users.noreply.github.com>
2020-10-03 01:58:09 -0600

    More practice

---
+++ CSFundamentals/CSFundamentals/Concepts/Algorithms/ExtraLongFactorials.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Numerics;
+
+namespace CSFundamentals.Concepts.Algorithms
+{
+    public class ExtraLongFactorials
+    {
+        /*
+         * Link to the problem: https://www.hackerrank.com/challenges/extra-long-factorials/problem
+         */
+
+        public static BigInteger ExtraLongFactorialsCalculate(int n)
+        {
+            var factorial = new BigInteger(n);
+
+            for (var i = n - 1; i > 0; i--)
+            {
+                factorial = factorial * i;
+            }
+
+            return factorial;
+        }
+
+        public static void ExtraLongFactorialsWorker() 
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(ExtraLongFactorialsCalculate(n));
+        }
+
+        public static BigInteger ExtraLongFactorialsRecursive(int n)
+        {
+            var factorial = new BigInteger(1);
+
+            if (n == 1)
+            {
+                return n;
+            }
+
+            factorial = n * ExtraLongFactorialsRecursive(n - 1);
+
+            return factorial;
+        }
+
+        // There is another way to do the big integer algorithm using strings
+        // Here are some explanations on how to do it
+        // https://www.geeksforgeeks.org/multiply-large-numbers-represented-as-strings/
+        // https://www.geeksforgeeks.org/sum-two-large-numbers/
+    }
+}
---
+++ CSFundamentals/CSFundamentals/Concepts/ArraysStrings/SimpleArraySum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace CSFundamentals.Concepts.ArraysStrings
+{
+    public class SimpleArraySum
+    {
+        /*
+         * Link to the problem: https://www.hackerrank.com/challenges/simple-array-sum/problem
+         */
+
+        public static int SimpleArraySumLong(int[] ar)
+        {
+            var sum = 0;
+
+            foreach (var arVal in ar)
+            {
+                sum += arVal;
+            }
+
+            return sum;
+        }
+
+        public static int SimpleArraySumUsingArraySum(int[] ar)
+        {
+            return ar.Sum(); // But this is using Linq
+        }
+    }
+}
