a81d5e1856570fa327996fb86d4c9c50e5a8f99f
Yessy Eckley <30677117+YessyEckley@users.noreply.github.com>
2020-10-11 01:35:14 -0600

    Trees

--- CSFundamentals/CSFundamentals/Concepts/StackQueues/TowerOfHanoi.cs
+++ CSFundamentals/CSFundamentals/Concepts/StackQueues/TowerOfHanoi.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace CSFundamentals.Concepts.StackQueues
 {
     public class TowerOfHanoi
@@ -8,8 +10,46 @@ namespace CSFundamentals.Concepts.StackQueues
          * The example is about the Tower of Hanoi on page 86
          */
 
-        public TowerOfHanoi()
+        public int DiscsCount { get; private set; }
+        public int MovesCount { get; private set; }
+        public Stack<int> From { get; private set; }
+        public Stack<int> To { get; private set; }
+        public Stack<int> Auxiliary { get; private set; }
+        public event EventHandler<EventArgs> MoveCompleted;
+
+        public TowerOfHanoi(int discs)
         {
+            DiscsCount = discs;
+            From = new Stack<int>();
+            To = new Stack<int>();
+            Auxiliary = new Stack<int>();
+
+            for (int i = 1; i <= discs; i++)
+            {
+                int size = discs - 1 + 1;
+                From.Push(size);
+            }
         }
+
+        public void Start()
+        {
+            Move(DiscsCount, From, To, Auxiliary);
+        }
+
+        public void Move(int discs, Stack<int> from, Stack<int> to, Stack<int> auxiliary)
+        {
+            if (discs > 0)
+            {
+                Move(discs - 1, from, auxiliary, to);
+
+                to.Push(from.Pop());
+                MovesCount++;
+                MoveCompleted?.Invoke(this, EventArgs.Empty);
+
+                Move(discs - 1, auxiliary, to, from);
+            }
+        }
+
+        // TODO: Finish the exercise
     }
 }
---
+++ CSFundamentals/CSFundamentals/Concepts/TreesHeapsGraphs/BasicTreeConcept.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CSFundamentals.Concepts.TreesHeapsGraphs
+{
+    public class BasicTreeConcept
+    {
+        public class TreeNode<T>
+        {
+            public T Data { get; set; }
+            public TreeNode<T> Parent { get; set; }
+            public List<TreeNode<T>> Children { get; set; }
+
+            public int GetHeight()
+            {
+                int height = 1;
+                var current = this;
+
+                while (current.Parent != null)
+                {
+                    height++;
+                    current = current.Parent;
+                }
+
+                return height;
+            }
+        }
+
+        public class Tree<T>
+        {
+            public TreeNode<T> Root { get; set; }
+        }
+
+        public void BasicTreeWorker()
+        {
+            var tree = new Tree<int>();
+            tree.Root = new TreeNode<int> { Data = 100 };
+
+            tree.Root.Children = new List<TreeNode<int>>
+            {
+                new TreeNode<int>{ Data = 50, Parent = tree.Root },
+                new TreeNode<int>{ Data = 1, Parent = tree.Root },
+                new TreeNode<int>{ Data = 150, Parent = tree.Root },
+            };
+
+            tree.Root.Children[2].Children = new List<TreeNode<int>>
+            {
+                new TreeNode<int>{ Data = 30, Parent = tree.Root.Children[2] }
+            };
+        }
+    }
+
+    public class BinaryTreeConcept
+    {
+        public class TreeNode<T>
+        {
+            public T Data { get; set; }
+            public TreeNode<T> Parent { get; set; }
+            public List<TreeNode<T>> Children { get; set; }
+
+            public int GetHeight()
+            {
+                var height = 1;
+                var current = this;
+
+                while (current.Parent != null)
+                {
+                    height++;
+                    current = current.Parent;
+                }
+
+                return height;
+            }
+        }
+
+        public class BinaryTreeNode<T> : TreeNode<T>
+        {
+            public BinaryTreeNode<T> Left
+            {
+                get { return (BinaryTreeNode<T>)Children[0]; }
+                set { Children[0] = value; }
+            }
+
+            public BinaryTreeNode<T> Right
+            {
+                get { return (BinaryTreeNode<T>)Children[1]; }
+                set { Children[1] = value; }
+            }
+
+            public BinaryTreeNode() => Children = new List<TreeNode<T>> { null, null };
+        }
+
+        public class BinaryTree<T>
+        {
+            public BinaryTreeNode<T> Root { get; set; }
+            public int Count { get; set; }
+        }
+    }
+}
