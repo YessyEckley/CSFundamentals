634968890d55852374b7c5794e265b935d25c4aa
Yessy Eckley <30677117+YessyEckley@users.noreply.github.com>
2020-12-10 14:45:05 -0700

    BitapAlgo

---
+++ CSFundamentals/CSFundamentals/Concepts/Algorithms/BitapAlgorithm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace CSFundamentals.Concepts.Algorithms
+{
+    public class BitapAlgorithm
+    {
+        /*
+         * Resource: https://en.wikipedia.org/wiki/Bitap_algorithm
+         * 
+         * Other: https://stackoverflow.com/questions/19569441/returning-most-relevant-search-results-using-linq-where-take
+         *        https://en.wikipedia.org/wiki/Approximate_string_matching
+         *        https://en.wikipedia.org/wiki/String-searching_algorithm
+         *        https://www.codeproject.com/Articles/36869/Fuzzy-Search
+         *        https://docs.microsoft.com/en-us/sql/relational-databases/system-functions/containstable-transact-sql?view=sql-server-ver15
+         */
+
+        public BitapAlgorithm()
+        {
+        }
+    }
+}
